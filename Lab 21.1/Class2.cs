8f2c3e10d941be331d366ef1ab4e4d543565a20f
Vladislav0121030 <113757794+Vladislav0121030@users.noreply.github.com>
2022-11-05 11:53:03 +0700

    Добавлен новый файл

---
+++ Lab 21.1/Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_21._1
+{
+    class Exam
+    {
+        public string Name;
+        public int Rating;
+        public DateTime _Exam;
+
+        public Exam(string Name, int Rating, DateTime Exam)
+        {
+            
+        }
+        public Exam()
+        {
+            Name = " ";
+            Rating = 1;
+            var Dexam = new DateTime(2001, 01, 01);
+        }
+    }
+    class Student
+    {
+        private Person _Sdata;
+        private string _Lform;
+        private int _Ngroup;
+        private Exam _Iexam;
+
+    }
+}
